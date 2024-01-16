ba900d894b81b29dba4c617b6d9741cea2735cbe
Tim <timythuang@gamania.com>
2024-01-16 17:04:56 +0800

    Update index.html

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function initializeLiff(liffId) {
-    liff.init({
-        liffId: liffId
-    }).then(() => {
-        console.log(liff.isLoggedIn()); 
-    }).catch(err => {
-        console.error('LIFF Initialization failed', err);
-    });
-}
+liff.init({
+    liffId: '2002831974-oaMLXlv9'
+}).then(() => {
+    if (liff.isLoggedIn()) {
+        liff.getProfile().then(profile => {
+            document.getElementById('displayName').textContent = '暱稱: ' + profile.displayName;
+            document.getElementById('pictureUrl').src = profile.pictureUrl;
+            document.getElementById('userId').textContent = '用戶ID: ' + profile.userId;
+            document.getElementById('statusMessage').textContent = '狀態消息: ' + profile.statusMessage;
+        }).catch(err => {
+            console.error('獲取用戶資料失敗', err);
+        });
+    } else {
+        // 用戶未登入，引導用戶登入
+        liff.login();
+    }
+}).catch(err => {
+    console.error('LIFF Initialization failed', err);
+});
